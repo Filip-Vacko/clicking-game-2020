156917e79372052e593afe9ad1d995e0e4295151
filip-vacko <filip.vacko@gmail.com>
2020-05-24 16:57:48 +0200

    make a random colored square appear on a page reload

---
+++ script.js
@@ -0,0 +1,25 @@
+function getRandomColor () {
+   
+    let letters = "0123456789ABCDEF";
+
+    let color = "#";
+
+    for (let i = 0; i <6; i++) {
+
+        color+= letters[Math.floor(Math.random() * 16)];
+
+    }
+
+    document.getElementById("shape-div").style.backgroundColor = color;
+
+}
+
+getRandomColor ();
+
+
+
+
+
+
+
+
